97acefdb161f6fcfbcdd9531b9238c588ea37ca5
Gopala <agrawalgopala@gmail.com>
2022-01-06 10:43:29 +0530

    query id add

--- task/pages/[id]/task.tsx
+++ task/pages/[id]/task.tsx
@@ -4,18 +4,18 @@ import {useRouter} from 'next/router';
 const Task = () => {
     const router = useRouter();
     
-    const [id , setId] = useState<string | string[] | undefined>('0');
-    const queryId = router.query.id;
-    console.log(router.query.id);
-    useEffect(() => {
-        setId(queryId)
-        console.log('id use', id);
-    }, [queryId])
+    // const [id , setId] = useState<string | string[] | undefined>('0');
+    // const queryId = router.query.id;
+    // console.log(router.query.id);
+    // useEffect(() => {
+    //     setId(queryId)
+    //     console.log('id use', id);
+    // }, [queryId])
 
-    console.log('id', id)
+    // console.log('id', id)
     return (
         <div>
-            Task {id} - {queryId} - {router.query.id}
+            Task - {router.query.id}
         </div>
     )
 }
