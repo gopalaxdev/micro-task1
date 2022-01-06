82808a7b654ab2b7dbb0ac507ac63cde75718e94
Gopala <agrawalgopala@gmail.com>
2022-01-06 09:59:41 +0530

    query id

--- task/pages/[id]/task.tsx
+++ task/pages/[id]/task.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useRouter} from 'next/router';
 
 const Task = () => {
     const router = useRouter();
-
+    
+    const [id , setId] = useState<string | string[] | undefined>('0');
+    const queryId = router.query.id;
     console.log(router.query.id);
+    setId(queryId);
     return (
         <div>
-            Task {router.query.id}
+            Task {id}
         </div>
     )
 }
